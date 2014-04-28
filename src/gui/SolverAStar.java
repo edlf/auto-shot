b81ee7d19f2c1d5d6c66b05726006246c573926f
Eduardo Fernandes <e.luis@icloud.com>
2014-04-28 11:09:01 +0100

    Add AStar Solver class

---
+++ src/gui/SolverAStar.java
@@ -0,0 +1,69 @@
+package gui;
+
+/**
+ * A* Algorithm Solver
+ * Eduardo Fernandes
+ */
+public class SolverAStar extends Solver {
+    private int numberOfMovesTried;
+    private int numberOfBackTracks;
+
+    SolverAStar(GameBoard input) throws Exception{
+        super(input);
+        numberOfMovesTried = 0;
+        numberOfBackTracks = 0;
+    }
+
+    public void searchSolution() throws Exception{
+        initializeSolver();
+        if (!solutionFound) {
+            searchSolutionAux();
+        }
+        hasRun = true;
+    }
+
+    /* TODO Adapt to AStar */
+    private void searchSolutionAux() throws Exception{
+        /* Try available moves */
+        for (int i = 0; i < gameBoard.getNumberOfAvailableMoves(); i++) {
+            attemptMove(gameBoard.getAvailableMoves().get(i));
+            solutionFound = gameBoard.isBoardSolved();
+
+            /* Stop recursion if the solution has been found */
+            if (solutionFound) {
+                return;
+            }
+
+            searchSolutionAux();
+        }
+
+        /* No more moves and no solution? backtrack */
+        if (!solutionFound) {
+            backtrack();
+        }
+    }
+
+    private void backtrack(){
+        gameBoard.undoMove();
+        moveStack.pop();
+        numberOfBackTracks++;
+    }
+
+    private void attemptMove(GameMove in) throws Exception {
+        moveStack.push(in);
+        gameBoard.doMove(in, false);
+        numberOfMovesTried++;
+    }
+
+    @Override
+    public void printStatistics(){
+        System.out.println("Number of backtracks: " + Integer.toString(numberOfBackTracks));
+        System.out.println("Number of moves tried: " + Integer.toString(numberOfMovesTried));
+    }
+
+    @Override
+    public String toString(){
+        return "A* Algorithm";
+    }
+
+}
